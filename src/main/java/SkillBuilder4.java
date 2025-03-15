242ccc8e4d8e55e9359ba07fef2117c9631303f5
ItEdgehill <starwarsfan0125@gmail.com>
2025-03-14 23:00:52 -0400

    Complete!

--- src/main/java/SkillBuilder4.java
+++ src/main/java/SkillBuilder4.java
@@ -10,9 +10,20 @@
 public class SkillBuilder4 {
 
     // replace this line with your constants
+    final private static String cht = "tT";
+    final private static String chy = "yY";
 
     public static String findTYPattern(String s)
     {
         // replace this line with your code
+        String result = "";
+        int tIndex = s.toLowerCase().indexOf("t");
+        int yIndex = s.toLowerCase().indexOf("y");
+
+        if(tIndex != -1 && yIndex != -1 && yIndex > tIndex){
+            result = s.substring(tIndex, yIndex + 1);
+        }
+
+        return result;
     }
 }
